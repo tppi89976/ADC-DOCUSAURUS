7043187628712997970077eb92cb2515fd7b0ac2
tppi89976 <a89976@ualg.pt>
2025-11-24 16:37:30 +0000

    titulo alterado e tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Site do Tiago',
+  tagline: 'Crazy',
   favicon: 'img/favicon.ico',
 // ...on.ico',
 
