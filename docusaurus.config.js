d5d0ed6713ac577626149894723016109c960ac8
tppi89976 <a89976@ualg.pt>
2025-11-24 15:27:46 +0000

    tppi fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'tpi89976', // Usually your GitHub org/user name.
+  organizationName: 'tppi89976', // Usually your GitHub org/user name.
   projectName: 'ADC-DOCUSAURUS', // Usually your repo name.
   deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
